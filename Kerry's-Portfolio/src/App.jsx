4e03565c9b854c7bd3955e74097029e13958bece
Carrying Little <fgv2yf@virginia.edu>
2025-03-09 14:38:29 -0400

    I finished my website portfolio's design. I just need to add edits to tailor it to myself.

--- Kerry's-Portfolio/src/App.jsx
+++ Kerry's-Portfolio/src/App.jsx
@@ -2,14 +2,25 @@ import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Myself } from './components/Myself/Myself';
 import { AboutMe } from './components/AboutMe/AboutMe';
+import { Experience } from './components/Experience/Experience';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
 
 function App() {
   
   return (<div className={styles.App}>
-    <Navbar/>
-    <Myself/>
-    <AboutMe/>
+    <div className={styles.scrollBars}>
+      <div style={{overflowX: 'hidden', overflowY: 'auto'}}>
+        <Navbar/>
+        <Myself/>
+        <AboutMe/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+      </div>
     </div>
+  </div>
+    
   );
 }
 
---
+++ Kerry's-Portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact Information</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:kerryyaoxiao@outlook.com">Email</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/kerry-xiao">Linkedin</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/AuthorizedTree">Github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
