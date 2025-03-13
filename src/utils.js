be0ab70dc3725f3d4d7bb4dc1aaddb50a983aad0
Carrying Little <fgv2yf@virginia.edu>
2025-03-12 23:02:19 -0400

    ran some tests

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
 export const getImageUrl = (path) => {
     //return new URL(`/assets/${path}`, import.meta.url).href;
-    return `/assets/${path}`;
+    /*return `/assets/${path}`;*/
+    /*return `/public/assets/${path}`;*/
+    return `/src/assets/${path}`;
 };
