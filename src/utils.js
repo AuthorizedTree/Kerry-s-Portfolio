d2f858ce6b373798da93043d64dda394caca1fb6
Carrying Little <fgv2yf@virginia.edu>
2025-03-12 23:28:11 -0400

    I updated the utils.js file so the getImageUrl function would work properly.

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,6 @@ export const getImageUrl = (path) => {
     //return new URL(`/assets/${path}`, import.meta.url).href;
     /*return `/assets/${path}`;*/
     /*return `/public/assets/${path}`;*/
-    return `/src/assets/${path}`;
+    const imgUrl = new URL([`../assets/${path}`], import.meta.url).href;
+    return imgUrl;
 };
