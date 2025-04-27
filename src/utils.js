e267caedc99d2f67083d2c32438cc398e529a65f
Carrying Little <fgv2yf@virginia.edu>
2025-04-27 17:35:41 -0400

    I changed the navbar images.

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const getImageUrl = (path) => {
-    return new URL([`../assets/${path}`], import.meta.url).href;
+    return new URL([`../public/assets/${path}`], import.meta.url).href;
 
     /*
     const images = import.meta.glob('../../assets/nav/*.png', { eager: true });
