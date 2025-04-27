18ccc3d386c5d6fa61dfedfe748b6128e92e3be9
Carrying Little <fgv2yf@virginia.edu>
2025-04-27 16:23:43 -0400

    I edited the utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,39 @@
 export const getImageUrl = (path) => {
+    if (process.env.NODE_ENV === 'production') {
+        return `/assets/${path}`;
+    }
+    return new URL([`../assets/${path}`], import.meta.url).href;
+
+    /*
+    const images = import.meta.glob('../../assets/nav/*.png', { eager: true });
+    const imagePath = `../../assets/nav/${path}`;
+    return images[imagePath]?.default;
+    */
+}
+/*
+export default function getImageUrl(asset) {
+    const assets = import.meta.glob('/assets/${path}', {eager: true});
+  
+    const getImageUrl = () => {
+     if (assets[asset]) {
+       return assets[asset].default
+     }
+    }
+  
+    return getImageUrl()
+}
+*/
+
+//export const getImageUrl = (path) => {
     //return new URL(`/assets/${path}`, import.meta.url).href;
     /*return `/assets/${path}`;*/
     /*return `/public/assets/${path}`;*/
+    //const path = `/src/assets/${path}`;
+    //const modules = import.meta.glob("/assets/*", { eager: true });
+    /*const mod = modules[path] as { default: string };*/
+    //return mod.default;
+    /*
     const imgUrl = new URL([`/assets/${path}`], import.meta.url).href;
     return imgUrl;
-};
+    */
+//};
